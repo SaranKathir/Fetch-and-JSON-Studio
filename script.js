c547df14b51878ceba60b3df7c24363602a7e124
Saran <saran.balu@outlook.com>
2022-08-12 10:49:52 -0500

    linked css styles

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+       // console.log(response.json());
+       response.json().then(function(json){
+    //console.log(json[0]);
+        const div=document.getElementById("container");
+        let htmlstring = '';
+        for(let i=0;i<json.length;i++){
+        htmlstring += `
+       <div class ="astronaut">
+    
+       <h3>${json[i].firstName} ${json[i].lastName}</h3>
+       <ul>
+            <li>HoursInSpace:${json[i].hoursInSpace}</li>
+            <li>Active:${json[i].active}</li>
+            <li>Skills:${json[i].skills}</li>
+        </ul>
+        <img class="avatar" src="${json[i].picture}"></img>
+        </div>
+       
+                    `;
+        }
+        div.innerHTML = htmlstring;
+
+       })
+    })
+ });// TODO: add code here
